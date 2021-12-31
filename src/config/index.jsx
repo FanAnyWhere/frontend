17755043691d6a4599512d60584a9887f15574a6
shivani__prajapati <shivani.prajapati@minddeft.com>
2021-12-31 13:40:12 +0530

    Changes the url of server

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // backend api url
 export const api = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  // baseURL: 'http://localhost:4000/api/v1', // Local system url
+  baseURL: 'http://ec2-18-191-95-183.us-east-2.compute.amazonaws.com:4000/api/v1', // Server url 
 });
 
 // web3 data for polygon(mumbai) testnet
