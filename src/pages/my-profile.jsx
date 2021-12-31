c3a8f52e3d53fa9bf1e56523221a0bf234cb5d76
Bansi Garala <bansi.garala@minddeft.net>
2021-12-31 15:52:14 +0530

    profile page changes

--- src/pages/my-profile.jsx
+++ src/pages/my-profile.jsx
@@ -1,16 +1,12 @@
 import React, { Component, useState } from 'react';
 import styled from 'styled-components';
 import Gs from '../theme/globalStyles';
-import Collapse from '@kunukn/react-collapse';
 import { Link, NavLink } from 'react-router-dom';
-import { AiOutlineHeart } from 'react-icons/ai';
-import { HiOutlineChevronDown } from 'react-icons/hi';
-import { IoCloseSharp } from 'react-icons/io5';
-import Collapsible from 'react-collapsible';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import { MdOutlineContentCopy } from 'react-icons/md'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 import ProfileCoverImg from '../assets/images/profile-cover.jpg';
+import ProfileImg from '../assets/images/nft-5.jpg';
 
 
 function MyProfile(props) {
@@ -22,6 +18,19 @@ function MyProfile(props) {
           <img src={ProfileCoverImg} alt='' />
         </div>
       </ProfileCover>
+      <ProfileRow>
+        <PRLeft>
+          <div className='image-outer'>
+            <img src={ProfileImg} alt='' />
+          </div>
+        </PRLeft>
+        <PRRight>
+          <PTitle>Profile Name Lorem Ipsum</PTitle>
+          <AddressBar><p>0htxas4...09jh938sx</p>
+            <MdOutlineContentCopy />
+          </AddressBar>
+        </PRRight>
+      </ProfileRow>
     </>
   );
 }
@@ -35,6 +44,31 @@ const ProfileCover = styled.div`
   .img-outer{width:100%; height:250px; overflow:hidden; border-radius:5px;
     img{width:100%; height:100%; object-fit:cover;}
   }
+`;
+
+const ProfileRow = styled(FlexDiv)`
+  justify-content:flex-start;
+`;
+
+const PRLeft = styled.div`
+  margin-left:54px; margin-right:42px;
+  .image-outer{width:200px; height:200px; overflow:hidden; border-radius:50%;
+    img{width:100%; height:100%; object-fit:cover;}
+  }
+`;
+
+const AddressBar = styled(FlexDiv)`
+  justify-content:flex-start; margin-bottom:17px;
+  p{
+    margin:0px; font-family: 'Roboto', sans-serif; font-weight: normal; font-size: 10px; line-height: 16px; color: #F6F6F6; background: rgba(196, 196, 196, 0.15); border-radius: 10px; padding:2px 10px;
+  }
+  svg{margin-left:10px; cursor:pointer;}
+`;
+
+const PRRight = styled.div``;
+
+const PTitle = styled.div`
+  font-weight: bold; font-size: 24px; line-height: 24px; color: #FFFFFF; margin:0px 0px 12px;
 `;
 
 export default MyProfile;
