c53f82d49f4c4394ae3a71a5191b3cef51951866
Bansi-Garala <76941786+Bansi-Garala@users.noreply.github.com>
2022-01-06 19:37:32 +0530

    collections page added

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const MyProfile = React.lazy(() => import('./pages/my-profile'))
 const EditProfile = React.lazy(() => import('./pages/edit-profile'))
 const NFTDetail = React.lazy(() => import('./pages/nft-detail'))
 const CreateNFT = React.lazy(() => import('./pages/create-nft'))
+const Collections = React.lazy(() => import('./pages/collections'))
 
 function App() {
 
@@ -40,6 +41,7 @@ function App() {
             <Route path="/edit-profile"><EditProfile /></Route>
             <Route path="/nft-detail/:id"><NFTDetail /></Route>
             <Route path="/create-nft"><CreateNFT /></Route>
+            <Route path="/collections"><Collections /></Route>
             <Route component={NotFound} />
           </Switch>
           <Footer />
