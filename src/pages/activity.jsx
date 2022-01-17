88c34c417428b15282185d98473f78112b95f60f
Bansi Garala <bansi.garala@minddeft.net>
2022-01-17 19:18:26 +0530

    activity page responsive added

--- src/pages/activity.jsx
+++ src/pages/activity.jsx
@@ -5,7 +5,8 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { Link } from 'react-router-dom';
 import { IoCloseSharp } from 'react-icons/io5';
-import 'react-loading-skeleton/dist/skeleton.css'
+import 'react-loading-skeleton/dist/skeleton.css';
+import Media from '../theme/media-breackpoint';
 
 import User1 from '../assets/images/user-1.png'
 import User2 from '../assets/images/user-2.png'
@@ -134,25 +135,37 @@ const ActTitle = styled.div`
 
 const ActOuter = styled(FlexDiv)`
   align-items:flex-start;
+  ${Media.md} {
+    flex-direction: column-reverse;
+  }
 `;
 
 const ActLeft = styled.div`
   width:calc(66% - 40px); margin-right:40px; 
+  ${Media.md} {
+    width:100%; margin-right:0px;
+  }
 `;
 
 const ActRight = styled.div`
-  width:34%
+  width:34%;
+  ${Media.md} {
+    width:100%;
+  }
 `;
 
 const ActFilterList = styled(FlexDiv)`
   justify-content:flex-start; margin-bottom:54px;
   a{font-weight: bold; font-size: 16px; line-height: 24px; color: #767676; padding:1px 18px; border-bottom:2px solid #767676;
     &.active{color:#fff; border-color:#fff;}
+    ${Media.md} {
+      padding:1px 15px;
+    }
   }
 `;
 
 const NotifiList = styled(FlexDiv)`
-  justify-content:flex-start; position:relative; background: #2F2F2F; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.25); border-radius: 5px; padding:9px 16px; margin:0px 0px 20px;
+  flex-wrap:nowrap; justify-content:flex-start; position:relative; background: #2F2F2F; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.25); border-radius: 5px; padding:9px 16px; margin:0px 0px 20px;
   img{margin-right:16px; width:50px; height:50px; border-radius:50%; object-fit:cover;}
   svg{font-size:28px; color:#767676; position:absolute; top:10px; right:8px; cursor:pointer;
     :hover{opacity: 0.8;}
@@ -182,6 +195,9 @@ const NotifiTitleBar = styled(FlexDiv)`
   a{font-weight: bold; font-size: 12px; line-height: 16px; color: #824CF5; border-bottom:0px; margin:0px;
     :hover{background: linear-gradient(92.95deg, #824CF5 0.8%, #0FBFFC 103.91%); -webkit-background-clip: text; -webkit-text-fill-color: transparent;}
   }
+  ${Media.md} {
+    margin-bottom:30px;
+  }
 `;
 
 const NTitle = styled.div`
@@ -199,6 +215,9 @@ const FilterTags = styled(FlexDiv)`
     &.active{background: linear-gradient(92.95deg, #824CF5 0.8%, #0FBFFC 103.91%); color:#fff;
       span{background: linear-gradient(92.95deg, #824CF5 0.8%, #0FBFFC 103.91%);}
     }
+  }
+  ${Media.md} {
+    margin-bottom:30px;
   }
 `;
 
