4613966d2899e6aa82b3f469d39ea5797b9a9187
shivani__prajapati <shivani.prajapati@minddeft.com>
2022-01-25 15:31:38 +0530

    reverted profile default cover photo chanegs

--- src/pages/my-profile.jsx
+++ src/pages/my-profile.jsx
@@ -29,8 +29,6 @@ import ArrowUp from '../assets/images/arrow-up.png';
 import SearchWhiteIcon from '../assets/images/search-white.png';
 import EditIcon from '../assets/images/edit-icon.png';
 import CopyIcon from '../assets/images/copy.png';
-import CreatorCImg from '../assets/images/creator-cover.jpg';
-import CreatorPImg from '../assets/images/creator-profile.png';
 import TwitterIcon1 from '../assets/images/twitter.png';
 import FacebookIcon1 from '../assets/images/facebook.png';
 import ExclaimIcon from '../assets/images/exclamation.png';
@@ -240,7 +238,7 @@ function MyProfile(props) {
     <>
       <ProfileCover>
         <div className='img-outer'>
-          <img src={props.user?.cover ? props.user.cover: CreatorCImg} alt='' />
+          <img src={props.user?.cover} alt='' />
           <div className='overlay'>
             {loading && <SiteLoader>
               <div className='loader-inner'>
@@ -271,7 +269,7 @@ function MyProfile(props) {
       <ProfileRow>
         <PRLeft className='desktop-profile'>
           <div className='image-outer'>
-            <img src={props.user?.profile ? props.user.profile: CreatorPImg} alt='' />
+            <img src={props.user?.profile} alt='' />
             <div className='overlay'>
               {/* <SiteLoader>
                 <div className='loader-inner'>
