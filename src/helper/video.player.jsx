dfb49ffbca5ccefa06c6151b2289849b6624aa22
shivani__prajapati <shivani.prajapati@minddeft.com>
2022-02-01 15:10:10 +0530

    stop sutoplay video for other pages

--- src/helper/video.player.jsx
+++ src/helper/video.player.jsx
@@ -1,18 +1,31 @@
+import { useEffect, useRef } from 'react';
 
 const VideoPlayer = (props) => {
+
+    let videoRef = useRef()
 
     const { url, image } = props
 
     const stopVideo = (e) => {
+        console.log('Off')
         e.target.pause();
+        e.target.currentTime = 0;
     }
     
     const playVideo = (e) => {
         e.target.play();
     }
 
+    useEffect(() => {
+        // to stop the auto play
+        var mouseOutEvent = document.createEvent('Events');
+        mouseOutEvent.initEvent('mouseout', true, false);
+        videoRef.dispatchEvent(mouseOutEvent)
+        // eslint-disable-next-line
+    })
+
     return (
-        <video 
+        <video ref={element => videoRef = element} 
             onMouseOver={(e) => playVideo(e)}
             onMouseOut={(e) => stopVideo(e)}
             style={{objectFit: 'cover', height: '100%', width: '100%'}}
@@ -21,7 +34,7 @@ const VideoPlayer = (props) => {
             // playsInline={true}
             autoPlay={true}
             loop
-            preload="auto" 
+            preload='auto' 
             muted={true}
         />
     )
